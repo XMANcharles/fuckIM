0f2ac2b0dc054ee18c71f5ecac692f0bc7907d8a
zhangcolder <965747597@qq.com>
2018-02-08 17:47:45 +0800

    预置我的账户数据

--- fuck/src/main/resources/db/data.sql
+++ fuck/src/main/resources/db/data.sql
@@ -2,6 +2,7 @@
 INSERT INTO user VALUES ('0001', '耿鑫1', 1, '我要打十个', 'http://tp1.sinaimg.cn/5619439268/180/40030060651/1','$2a$10$iPFXYw0maUa143FZlvfdLuBfGLSPtuKkVF5bU86USl4vVXL60fuuC');
 INSERT INTO user VALUES ('0002', '耿鑫2', 1, '我要打十个！', 'aaa.jpg','$2a$10$iPFXYw0maUa143FZlvfdLuBfGLSPtuKkVF5bU86USl4vVXL60fuuC');
 INSERT INTO user VALUES ('0003', '耿鑫3', 0, '我要打十个！', 'aaa.jpg','$2a$10$iPFXYw0maUa143FZlvfdLuBfGLSPtuKkVF5bU86USl4vVXL60fuuC');
+INSERT INTO user VALUES ('0004', 'zcodler', 1, 'Fuck! World!！', 'aaa.jpg','$2a$10$iPFXYw0maUa143FZlvfdLuBfGLSPtuKkVF5bU86USl4vVXL60fuuC');
 
 --ROLE
 INSERT INTO ROLE VALUES ('0001', '管理员');
@@ -12,10 +13,15 @@ INSERT INTO ROLE VALUES ('0003', 'USER');
 INSERT INTO USER_ROLE VALUES ('0001', '0001', '0001');
 INSERT INTO USER_ROLE VALUES ('0002', '0001', '0003');
 INSERT INTO USER_ROLE VALUES ('0003', '0002', '0003');
+INSERT INTO USER_ROLE VALUES ('0004', '0004', '0002');
 
 --USER_RELATION
 INSERT INTO user_relational VALUES ('0001', '0001', '0002','摇就完事了','000001','FF' );
 INSERT INTO user_relational VALUES ('0002', '0002', '0001','帅气小哥哥','000002','FF' );
+INSERT INTO user_relational VALUES ('0003', '0001', '0004','斯科达摩','000001','FF' );
+INSERT INTO user_relational VALUES ('0004', '0004', '0001','哈萨克斯','000001','FF' );
+
 
 --USER_GROUP
 INSERT INTO user_group VALUES ('000001','0001', '我渴望奶子');
+INSERT INTO user_group VALUES ('000002','0002', '正义必胜！');
